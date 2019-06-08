2dbf026cc0601b9c72ffc972135bc1bb516dfffd
Jonathan Hudak <hudak.jonathan@gmail.com>
2019-06-08 14:38:41 -0700

    AnniPlayer 2.0 Hooks Basic Setup and Demo

--- demo.js
+++ demo.js
@@ -1,13 +1,74 @@
-import ReactDOM from "react-dom";
-import React from "react";
-import MyComponent from ".";
+import 'babel-polyfill';
+import ReactDOM from 'react-dom';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Router, Link } from '@reach/router';
+import useCanvasScrubber from '.';
+import groundWorkFramesMap from 'image-sequence/the-groundwork/*.jpg';
+import magicFramesMap from 'image-sequence/magic/*.jpg';
+
+const groundWorkFrames = Object.values(groundWorkFramesMap);
+const magicFrames = Object.values(magicFramesMap);
+
+const canvasStyle = {
+  maxWidth: '100%'
+};
+
+function CanvasContainer({ children }) {
+  return <div style={{ position: 'relative', maxWidth: 800 }}>{children}</div>;
+}
+
+CanvasContainer.propTypes = {
+  children: PropTypes.node
+};
+
+const buttonStyle = {
+  padding: 16,
+  background: 'limegreen',
+  fontSize: 24
+};
+
+function Player({ frames }) {
+  const { canvasRef, togglePlay, isPlaying } = useCanvasScrubber({
+    frames
+  });
+  return (
+    <CanvasContainer>
+      <div style={{ padding: '8px 0 8px' }}>
+        <button onClick={togglePlay} style={buttonStyle}>
+          <span className={`fa ${isPlaying ? 'fa-pause' : 'fa-play'}`} />
+        </button>
+      </div>
+      <canvas style={canvasStyle} ref={canvasRef} />
+    </CanvasContainer>
+  );
+}
+
+function DemoA({ frames }) {
+  return <Player frames={frames} />;
+}
+
+function DemoB({ frames }) {
+  return <Player frames={frames} />;
+}
+
+Player.propTypes = {
+  frames: PropTypes.arrayOf(PropTypes.string)
+};
 
 function App() {
   return (
     <div>
-      <MyComponent />
+      <nav>
+        <Link to="/">home</Link>&nbsp;
+        <Link to="/magic">Magic</Link>
+      </nav>
+      <Router>
+        <DemoA path="/" frames={groundWorkFrames} />
+        <DemoB path="/magic" frames={magicFrames} />
+      </Router>
     </div>
   );
 }
 
-ReactDOM.render(<App />, root);
+ReactDOM.render(<App />, document.querySelector('#root'));
--- index.js
+++ index.js
@@ -1,7 +1,134 @@
-import React from 'react';
+import { useMemo, useRef, useReducer, useEffect } from 'react';
 
-function MyComponent() {
-  return React.createElement("div", null, "My Component");
+const DEFAULT_FPS = 24;
+
+const preloadImagePromise = src => new Promise((resolve, reject) => {
+  const img = new Image();
+
+  img.onload = () => resolve(img);
+
+  img.onerror = e => reject(e);
+
+  img.src = src;
+});
+
+const reducer = (state, action) => ({ ...state,
+  ...action
+});
+
+function extractFrameIndexFromPath(framePath) {
+  try {
+    const [fileName] = framePath.split('.');
+    const result = /([^._-])*\d/.exec(fileName);
+    const [frameIndex] = result;
+
+    if (!frameIndex) {
+      throw Error();
+    }
+
+    return frameIndex;
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(`${framePath} does not contain a valid frame index`);
+  }
 }
 
-export default MyComponent;
+function sortFramesAscending(frameA, frameB) {
+  const indexA = extractFrameIndexFromPath(frameA);
+  const indexB = extractFrameIndexFromPath(frameB);
+  return indexA - indexB;
+}
+
+function useCanvasScrubber({
+  fps = DEFAULT_FPS,
+  frames = []
+}) {
+  const sortedFrames = useMemo(() => frames.sort(sortFramesAscending), [frames]);
+  const currentFrame = useRef(null);
+  const nextTickRAF = useRef();
+  const canvasRef = useRef(null);
+  const [state, setState] = useReducer(reducer, {
+    currentFrame: 0,
+    isPlaying: false,
+    htmlImageElements: []
+  });
+  const {
+    isPlaying,
+    htmlImageElements
+  } = state;
+
+  function drawFrame(img) {
+    if (!img || !canvasRef.current) return;
+    canvasRef.current.width = img.width;
+    canvasRef.current.height = img.height;
+    canvasRef.current.getContext('2d').drawImage(img, 0, 0, img.width, img.height, 0, 0, img.width, img.height);
+  }
+
+  useEffect(() => {
+    if (!canvasRef) return;
+
+    async function load() {
+      const htmlImages = await Promise.all(sortedFrames.map(f => preloadImagePromise(f)));
+      if (!htmlImages || htmlImages.length === 0) return;
+      const {
+        width,
+        height
+      } = htmlImages[0];
+      setState({
+        htmlImageElements: htmlImages,
+        frameSize: {
+          width,
+          height
+        }
+      });
+      drawFrame(htmlImages[0]);
+    }
+
+    load();
+  }, [frames, canvasRef, sortedFrames]);
+  useEffect(() => {
+    if (isPlaying) {
+      if (!state.isPlaying) return;
+      let then = performance.now();
+      let now;
+      let delta;
+
+      const nextTick = () => {
+        if (!state.isPlaying) return;
+        now = performance.now();
+        delta = now - then;
+        const interval = Math.round(1000 / fps);
+
+        if (delta > interval) {
+          // Mutate next frame
+          const frameIndex = currentFrame.current;
+          then = now;
+          currentFrame.current = frameIndex === frames.length - 1 ? 0 : frameIndex + 1; // Draw Canvas
+
+          drawFrame(htmlImageElements[currentFrame.current]);
+        }
+
+        nextTickRAF.current = requestAnimationFrame(nextTick);
+      }; // START LOOP
+
+
+      nextTick();
+    } else if (nextTickRAF.current) {
+      cancelAnimationFrame(nextTickRAF.current);
+    }
+  }, [fps, frames.length, htmlImageElements, isPlaying, state.isPlaying]);
+
+  function togglePlay() {
+    const nextIsPlaying = htmlImageElements.length > 0 && !isPlaying;
+    setState({
+      isPlaying: nextIsPlaying
+    });
+  }
+
+  return { ...state,
+    canvasRef,
+    togglePlay
+  };
+}
+
+export default useCanvasScrubber;
